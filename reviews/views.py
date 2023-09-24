6c9e57886635f6c93e50fcb3efb420a6ec3fbace
Dannysesi <daniel.olusesi20@gmail.com>
2023-09-24 21:59:07 +0100

    views update

--- reviews/views.py
+++ reviews/views.py
@@ -2,4 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Hello, world!')
+    name = 'world'
+    return render(request, 'base.html', {'name': name})
+
+def search(request):
+    return render(request, 'search.html')
+
+
+def search_view(request):
+    # Step 2: Read the search string from URL parameters
+    search_query = request.GET.get('q', '')
+
+    # Step 3: Pass the search value to the template
+    context = {'search_query': search_query}
+
+    # Step 4: Render the template with the context
+    return render(request, 'search.html', context)
