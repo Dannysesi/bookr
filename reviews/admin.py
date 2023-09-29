26674a3efc8c733e92b78d683b3ac0948a3e8525
Dannysesi <daniel.olusesi20@gmail.com>
2023-09-29 23:03:04 +0100

    register models to admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+# class PublisherAdmin(admin.ModelAdmin):
+#     list_display = ['name', 'website', 'email']
+
+# admin.site.register(Publisher, PublisherAdmin)
+
+admin.site.register(Publisher)
+admin.site.register(Book)
+admin.site.register(Contributor)
+admin.site.register(BookContributor)
+admin.site.register(Review)
