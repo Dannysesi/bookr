8249e572c0a10db201dd0cf4c13918471e42524a
Dannysesi <daniel.olusesi20@gmail.com>
2023-10-03 21:19:32 +0100

    urls modification

--- bookr/urls.py
+++ bookr/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import reviews.views
+from reviews import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', reviews.views.index),
-    path('search/', reviews.views.search),
-    path('search_views/', reviews.views.search_view)
+    path('book-search/', reviews.views.search_view, name='book_search'),
+    path('', include('reviews.urls')),
 ]
