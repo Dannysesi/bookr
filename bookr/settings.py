378a9bcf2b0e71284be1599bd9091fa0d1ceaf8d
Dannysesi <daniel.olusesi20@gmail.com>
2023-10-09 11:45:29 +0100

    settings and url modification

--- bookr/settings.py
+++ bookr/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,8 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'reviews.apps.ReviewsConfig',
     'users.apps.UsersConfig',
-    'crispy_forms',
-    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -119,7 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- bookr/urls.py
+++ bookr/urls.py
@@ -20,6 +20,8 @@ from django.urls import path, include
 import reviews.views
 from reviews import views
 from users import views as users_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,7 +30,9 @@ urlpatterns = [
     path('profile/', users_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('book-search/', reviews.views.search_view, name='book_search'),
+    path('search/', reviews.views.search_view, name='search_view'),
     path('', include('reviews.urls')),
-    
-]
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
