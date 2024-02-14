2949721570e3ca4f9688a83b62c69b86b44e5d80
Dannysesi <daniel.olusesi20@gmail.com>
2024-02-14 19:31:26 +0100

    url routing

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
+from .views import BookCreateView, ContributorCreateView, BookUpdateView
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    # path('book_list/', views.book_list, name='book_list'),
     path('book_detail/<int:id>/', views.book_detail, name='book_detail'),
-    path('book_detail/', views.book_detail, name='book_detail')
+    path('book/new/', BookCreateView.as_view(), name='book-create'),
+    path('book/new/contributor', ContributorCreateView.as_view(), name='contributor-create'),
+    path('book_detail/<int:pk>/update/', BookUpdateView.as_view(), name='book-update')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
