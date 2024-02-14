bce97e3a8a3f766adb5a4960a1d8f0e18e5e2163
Dannysesi <daniel.olusesi20@gmail.com>
2024-02-14 19:31:13 +0100

    forms update

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,4 +1,30 @@
 from django import forms
+from .models import *
 
 class BookSearchForm(forms.Form):
     query = forms.CharField(label='Search', max_length=100)
+
+
+
+class BookForm(forms.ModelForm):
+    contributors = forms.ModelMultipleChoiceField(queryset=Contributor.objects.all(), required=False, widget=forms.CheckboxSelectMultiple)
+
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'publisher', 'contributors', 'isbn', 'publication_date']
+        widgets = {
+            'publication_date': forms.DateInput(attrs={'type': 'date'}),
+            'contributors': forms.SelectMultiple(attrs={'class': 'form-select'}),
+        }
+
+
+class BookImageForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['image']
+
+
+class ContributorForm(forms.ModelForm):
+    class Meta:
+        model = Contributor
+        fields = ['first_names', 'last_names', 'email']
